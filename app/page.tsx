5fcc900762fa939958dbbb55a9dd233a4b35fdf9
Nathan Brodin <nathan@brodin.dev>
2024-05-10 18:53:21 +0200

    feat: implement first component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import AmountCard from "@/components/amount-card";
 import { ThemeCustomizer } from "@/components/theme/theme-customizer";
+import { DollarSign } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-12">
       <ThemeCustomizer />
+      <AmountCard
+        title="Balance"
+        icon={DollarSign}
+        amount={1200}
+        diff="+20.1% from last month"
+      />
     </main>
   );
 }
