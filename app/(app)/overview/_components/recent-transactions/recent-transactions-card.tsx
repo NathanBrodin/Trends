b57227c6e3cf47e5ca5456b7ae1f0e3fe19504f4
Nathan Brodin <nathan@brodin.dev>
2024-05-15 21:12:03 +0200

    feat: implement draft of recent transactions

--- app/(app)/overview/_components/recent-transactions/recent-transactions-card.tsx
+++ app/(app)/overview/_components/recent-transactions/recent-transactions-card.tsx
@@ -6,6 +6,8 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { RecentTransaction } from "./columns";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 
 async function getData(): Promise<RecentTransaction[]> {
   return [
@@ -19,14 +21,6 @@ async function getData(): Promise<RecentTransaction[]> {
     },
     {
       id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "0",
       category: "House",
       name: "Rent",
       amount: 8000,
@@ -34,15 +28,7 @@ async function getData(): Promise<RecentTransaction[]> {
       bankAccountName: "Lydia",
     },
     {
-      id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "0",
+      id: "2",
       category: "House",
       name: "Rent",
       amount: 8000,
@@ -50,15 +36,7 @@ async function getData(): Promise<RecentTransaction[]> {
       bankAccountName: "Lydia",
     },
     {
-      id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "0",
+      id: "3",
       category: "House",
       name: "Rent",
       amount: 8000,
@@ -66,15 +44,7 @@ async function getData(): Promise<RecentTransaction[]> {
       bankAccountName: "Lydia",
     },
     {
-      id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "0",
+      id: "4",
       category: "House",
       name: "Rent",
       amount: 8000,
@@ -82,15 +52,7 @@ async function getData(): Promise<RecentTransaction[]> {
       bankAccountName: "Lydia",
     },
     {
-      id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "0",
+      id: "5",
       category: "House",
       name: "Rent",
       amount: 8000,
@@ -98,15 +60,7 @@ async function getData(): Promise<RecentTransaction[]> {
       bankAccountName: "Lydia",
     },
     {
-      id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "0",
+      id: "6",
       category: "House",
       name: "Rent",
       amount: 8000,
@@ -114,26 +68,10 @@ async function getData(): Promise<RecentTransaction[]> {
       bankAccountName: "Lydia",
     },
     {
-      id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "0",
+      id: "7",
       category: "House",
       name: "Rent",
       amount: 8000,
-      currency: "NOK",
-      bankAccountName: "Lydia",
-    },
-    {
-      id: "1",
-      category: "Food",
-      name: "Fly Chicken Storo",
-      amount: 199,
       currency: "NOK",
       bankAccountName: "Lydia",
     },
@@ -144,52 +82,36 @@ export async function RecentTransactionsCard() {
   const data = await getData();
 
   return (
-    <Card className="col-span-3 h-full">
-      <CardHeader>
-        <CardTitle className="text-lg">Recent Transactions</CardTitle>
-        <CardDescription>You spent a lot of money recently.</CardDescription>
+    <Card className="col-span-3 flex flex-col pb-2">
+      <CardHeader className="flex-row justify-between align-top p-6">
+        <div className="space-y-1.5">
+          <CardTitle className="text-lg">Recent Transactions</CardTitle>
+          <CardDescription>You spent a lot of money recently.</CardDescription>
+        </div>
+        <Button variant="outline" size="icon">
+          <PlusIcon />
+        </Button>
       </CardHeader>
-      <CardContent className="space-y-8">
-        <div className="flex items-center">
-          <div className="size-9 flex items-center justify-center rounded-full bg-muted">
-            H
-          </div>
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">House rent</p>
-            <p className="text-sm text-muted-foreground flex gap-1">
-              <span>14/05</span>
-              {" - "} <span>Lydia</span>
-            </p>
-          </div>
-          <div className="ml-auto font-medium">-NOK 8000</div>
-        </div>
-        <div className="flex items-center">
-          <div className="size-9 flex items-center justify-center rounded-full bg-muted">
-            F
-          </div>
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">
-              Fly Chicken Storo
-            </p>
-            <p className="text-sm text-muted-foreground flex gap-1">
-              <span>14/05</span>
-              {" - "} <span>Lydia</span>
-            </p>
-          </div>
-          <div className="ml-auto font-medium">-NOK 199</div>
-        </div>
-        <div className="flex items-center">
-          <div className="size-9 flex items-center justify-center rounded-full bg-muted">
-            S
-          </div>
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">Salary</p>
-            <p className="text-sm text-muted-foreground flex gap-1">
-              <span>14/05</span>
-              {" - "} <span>Lydia</span>
-            </p>
-          </div>
-          <div className="ml-auto font-medium">+NOK 60,234</div>
+      <CardContent className="px-0 flex-1 overflow-scroll">
+        <div className="max-h-0">
+          {data.map((transaction) => (
+            <div
+              key={transaction.id}
+              className="flex items-center transition-colors hover:bg-muted/50 py-3 px-6 rounded-lg"
+            >
+              <div className="size-9 flex items-center justify-center rounded-full bg-muted">
+                H
+              </div>
+              <div className="ml-4 space-y-1">
+                <p className="text-sm font-medium leading-none">House rent</p>
+                <p className="text-sm text-muted-foreground flex gap-1">
+                  <span>14/05</span>
+                  {" - "} <span>Lydia</span>
+                </p>
+              </div>
+              <div className="ml-auto font-medium">-NOK 8000</div>
+            </div>
+          ))}
         </div>
       </CardContent>
     </Card>
